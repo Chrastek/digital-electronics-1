702b82e4ade28e1186bf477128d420c946236298
Chrastek <kubachrastek@centrum.cz>
2023-03-23 12:24:19 +0100

    cv7

---
+++ 07-display_driver/display_driver.srcs/sim_1/new/cnt_up_down.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/23/2023 11:58:45 AM
+// Design Name: 
+// Module Name: cnt_up_down
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cnt_up_down(
+
+    );
+endmodule
